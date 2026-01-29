50c0f74bb3ab8835e2176ba79674711580fa98bb
pbr-319 <poreddybhargavareddy319@gmail.com>
2026-01-29 16:39:42 +0530

    day1js

---
+++ tasks/day1js/1.js
@@ -0,0 +1,53 @@
+// Arithmetric Operators
+let num1 = 15;
+let num2 = 5;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Modulus:", num1 % num2);
+
+// Assignment Operators
+let a = 5;
+let b = 3;
+
+a + b;
+console.log("a += b:",a);
+
+a - b;
+console.log("a -= b:",a);
+
+a * b;
+console.log("a *= b:",a);
+
+a / b;
+console.log("a /= b:",a);
+
+a % b;
+console.log("a %= b:",a);
+
+// Comparison Operators
+let x = 2;
+let y = 3;
+
+console.log("A:", x == y);
+console.log("B:", x != y);
+console.log("C:", x === y);
+console.log( "D:",x !== y);
+console.log("E:", x > y);
+console.log("F:", x >= y);
+console.log("G:", x < y);
+console.log("H", x <= y);
+
+// Logical Operators
+let age = 20;
+let ID = true;
+
+console.log("I:", age >= 18 && ID);
+console.log("J:", age < 18 || ID);
+console.log("K:", !ID);
+
+
+
+
