267ab8e3b86d2b5e8a2930f68dd62cb0912faa01
pbr-319 <poreddybhargavareddy319@gmail.com>
2026-01-30 09:05:27 +0530

    day2js code push

---
+++ tasks/day2js/2.js
@@ -0,0 +1,50 @@
+
+// if statement
+let temperature = 25;
+if (temperature = 20) {
+    console.log("Temp:","it was a warm day");
+}
+
+// if-else statement
+let hour = 16;
+if (hour < 18) {
+    console.log("Hour:","Good Day");
+} else {
+    console.log("Hour:","Good evening");
+}
+
+// else-if statement
+let speed = 80;
+if (speed > 120) {
+    console.log("Speed:","Over speed");
+} else if (speed > 60) {
+    console.log("Speed:","Normal speed");
+} else {
+    console.log("Speed:","Slow speed");
+}
+
+// if statement
+const num1= 5;
+
+if (num1 > 0) {
+    console.log("Number:", "positive");
+}
+
+// if-else statement
+const num2 = -10;
+
+if (num2 > 0) {
+    console.log("Number:", "positive.");
+} else {
+    console.log("Number:", "negative");
+}
+
+// else-if statement
+let Marks = 80;
+if (Marks >= 75) {
+    console.log("Result:","Distinction");
+} else if (Marks >45) {
+    console.log("Result:","Pass");
+} else {
+    console.log("Result:","Fail");
+}
