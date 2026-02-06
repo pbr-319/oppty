1eb3ebcb086ba6cb9c2f02c3a0192c304d9ad4a8
pbr-319 <poreddybhargavareddy319@gmail.com>
2026-02-06 18:07:59 +0530

    day4js code push

--- tasks/day7js/7.js
+++ tasks/day7js/7.js
@@ -1,19 +1,22 @@
 function happy() {
-    document.getElementById("text").style.color ="pink";
+    document.getElementById("text").style.color ="yellow";
     document.getElementById("text").innerText = "I Am Feeling Great. 😊 ";
 }
 
 function sad() {
+    
     document.getElementById("text").style.color ="purple";
     document.getElementById("text").innerText ="I Am Feeling Down Today. 😢 ";
 }
 
 function angry() {
+    
     document.getElementById("text").style.color ="red";
     document.getElementById("text").innerText ="I Am Frustrated. 😡 ";
 }
 
 function sleepy() {
+    
     document.getElementById("text").style.color ="green";
     document.getElementById("text").innerText ="I Need a Nap. 😴 ";
 }
