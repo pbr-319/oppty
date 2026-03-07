a43d874060152e73b39909496da638fe0329b917
pbr-319 <poreddybhargavareddy319@gmail.com>
2026-03-07 16:46:32 +0530

    code push

--- tasks/mar7/mar7.js
+++ tasks/mar7/mar7.js
@@ -1,83 +1,60 @@
-function showLogin(){
-
-registerBox.classList.add("hidden")
-loginBox.classList.remove("hidden")
-homeBox.classList.add("hidden")
-title.innerText="Login"
-
+function showLogin() {
+  registerBox.classList.add("hidden");
+  loginBox.classList.remove("hidden");
+  homeBox.classList.add("hidden");
+  title.innerText = "Login";
 }
 
-
-/* SHOW REGISTER */
-
-function showRegister(){
-
-loginBox.classList.add("hidden")
-registerBox.classList.remove("hidden")
-homeBox.classList.add("hidden")
-title.innerText="Register"
-
+function showRegister() {
+  loginBox.classList.add("hidden");
+  registerBox.classList.remove("hidden");
+  homeBox.classList.add("hidden");
+  title.innerText = "Register";
 }
 
+function register() {
+  let user = regUser.value;
+  let pass = regPass.value;
 
-/* REGISTER */
+  if (user === "" || pass === "") {
+    regMsg.innerText = "Fill all fields";
+    return;
+  }
 
-function register(){
+  localStorage.setItem(
+    "user",
+    JSON.stringify({
+      username: user,
+      password: pass,
+    }),
+  );
 
-let user=regUser.value
-let pass=regPass.value
-
-if(user==="" || pass===""){
-regMsg.innerText="Fill all fields"
-return
+  regMsg.innerText = "Registration Successful";
 }
 
-localStorage.setItem("user",JSON.stringify({
-username:user,
-password:pass
-}))
+function login() {
+  let user = loginUser.value;
+  let pass = loginPass.value;
 
-regMsg.innerText="Registration Successful"
+  let storedUser = JSON.parse(localStorage.getItem("user"));
 
+  if (user === storedUser.username && pass === storedUser.password) {
+    localStorage.setItem("loggedUser", user);
+
+    loginBox.classList.add("hidden");
+    homeBox.classList.remove("hidden");
+
+    welcome.innerText = "Welcome " + user;
+  } else {
+    loginMsg.innerText = "Invalid Username or Password";
+  }
 }
 
+function logout() {
+  localStorage.removeItem("loggedUser");
 
-/* LOGIN */
+  homeBox.classList.add("hidden");
+  loginBox.classList.remove("hidden");
 
-function login(){
-
-let user=loginUser.value
-let pass=loginPass.value
-
-let storedUser=JSON.parse(localStorage.getItem("user"))
-
-if(user===storedUser.username && pass===storedUser.password){
-
-localStorage.setItem("loggedUser",user)
-
-loginBox.classList.add("hidden")
-homeBox.classList.remove("hidden")
-
-welcome.innerText="Welcome "+user
-
-}else{
-
-loginMsg.innerText="Invalid Username or Password"
-
-}
-
-}
-
-
-/* LOGOUT */
-
-function logout(){
-
-localStorage.removeItem("loggedUser")
-
-homeBox.classList.add("hidden")
-loginBox.classList.remove("hidden")
-
-title.innerText="Login"
-
+  title.innerText = "Login";
 }
